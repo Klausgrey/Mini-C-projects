18ceaed00ce30ca389f1265e5abf207a181ced48
“Nicholas” <danielnick210@gmail.com>
2023-06-18 22:14:01 +0100

    my first real projet

---
+++ digital_clock/alt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h> // For sleep() function
+
+int main() {
+    while (1) {
+        // Get current time
+        time_t currentTime = time(NULL);
+        struct tm *timeInfo = localtime(&currentTime);
+
+        // Extract hours, minutes, and seconds
+        int hours = timeInfo->tm_hour;
+        int minutes = timeInfo->tm_min;
+        int seconds = timeInfo->tm_sec;
+
+        // Print the time in digital clock format
+        printf("%02d:%02d:%02d\n", hours, minutes, seconds);
+
+        // Delay for one second
+        sleep(1);
+    }
+
+    return 0;
+}
